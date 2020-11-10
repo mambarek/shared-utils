2ed86a553be27f9ab25e81d23c5d5f3e4e850ef0
Mohamed Ali Mbarek <mbarek@it-2go.de>
2020-11-10 12:35:19 +0100

    common entities added. Don't repeat your self

---
+++ src/main/java/com/it2go/util/jpa/entities/AddressEntity.java
@@ -0,0 +1,55 @@
+package com.it2go.util.jpa.entities;
+
+import java.io.Serializable;
+import java.util.UUID;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@SuppressWarnings("JpaDataSourceORMInspection")
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "ADDRESS")
+public class AddressEntity implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "PUBLIC_ID", unique = true, nullable = false)
+    private UUID publicId;
+
+    @Basic
+    @Column(name = "STREET_ONE", length = 100,nullable = false)
+    private String streetOne;
+
+    @Basic
+    @Column(name = "BUILDING_NR", length = 5, nullable = false)
+    private String buildingNr;
+
+    @Basic
+    @Column(name = "STREET_TWO", length = 100,nullable = true)
+    private String streetTwo;
+
+    @Basic
+    @Column(name = "ZIP_CODE", length = 10, nullable = false)
+    private String zipCode;
+
+    @Basic
+    @Column(name = "CITY", length = 100, nullable = false)
+    private String city;
+
+    @Basic
+    @Column(name = "COUNTRY_CODE", length = 3, nullable = false)
+    private String countryCode;
+}
---
+++ src/main/java/com/it2go/util/jpa/entities/PersonData.java
@@ -0,0 +1,42 @@
+package com.it2go.util.jpa.entities;
+
+import java.time.LocalDate;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * created by mmbarek on 10.11.2020.
+ */
+@Getter
+@Setter
+@Embeddable
+public class PersonData {
+
+  @Basic
+  @Column(name = "FIRST_NAME", nullable = false, length = 100)
+  private String firstName;
+
+  @Basic
+  @Column(name = "LAST_NAME", nullable = false, length = 100)
+  private String lastName;
+
+  @Basic
+  @Column(name = "BIRTH_DATE", columnDefinition = "DATE")
+  private LocalDate birthDate;
+
+  @Column(name = "GENDER", length = 10)
+  private String gender;
+
+  @Column(name = "EMAIL")
+  private String email;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(referencedColumnName = "PUBLIC_ID", name = "ADDR_ID")
+  private AddressEntity address;
+}
